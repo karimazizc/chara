a7b4a134cb0c8e0cc7cbf755e99692c7d2c202f2
karimazizc <karimazizc@gmail.com>
2025-06-27 00:27:07 +0700

    added project

---
+++ chara/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7*1-3y9y$8^sj+66z73bw9$8+2$fdg(+$p2d07a+03jzg3axnl"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
